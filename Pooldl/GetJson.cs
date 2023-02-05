e6595d5ccaf30f1214e5bcd58f903b7c3e3b5732
Vilagamer999 <vila@hackermail.com>
2023-02-05 22:57:26 +0000

    Add project files.

---
+++ Pooldl/GetJson.cs
@@ -0,0 +1,35 @@
+﻿namespace Pooldl
+{
+    internal class GetJson
+    {
+        public string name { get; set; }
+        public int[] post_ids { get; set; }
+        public string creator_name { get; set; }
+        public bool success { get; set; }
+    }
+
+    internal class Post
+    {
+        public string url { get; set; }
+        public string ext { get; set; }
+
+    }
+
+    //public class File
+    //{
+    //    public int width { get; set; }
+    //    public int height { get; set; }
+    //    public string ext { get; set; }
+    //    public int size { get; set; }
+    //    public string md5 { get; set; }
+    //    public string url { get; set; }
+    //}
+
+    public class Sample
+    {
+        public bool has { get; set; }
+        public int height { get; set; }
+        public int width { get; set; }
+        public string url { get; set; }
+    }
+}
---
+++ Pooldl/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Net;
+using System.Text.Json;
+using System.Threading;
+
+namespace Pooldl
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            userInput();
+        }
+
+        private static void userInput()
+        {
+            
+            Console.WriteLine("Please enter either:\n");
+            Console.WriteLine("- Pool URL / ID (e.g. https://e621.net/pools/8804)\n");
+            Console.WriteLine("- Pool ID (e.g. 7438) \n");
+            Console.WriteLine("- \"lucky\" (Will download a random pool) \n");
+            Console.Write("> ");
+            string input = Console.ReadLine();
+
+            //check if input is "lucky"
+            if (input == "lucky")
+            {
+                //get random pool id
+                Random rnd = new Random();
+                int poolID = rnd.Next(1, 32651);
+                Console.WriteLine("Downloading pool: " + poolID);
+                getPool(poolID);
+
+            }
+            else
+            {
+                //check if input is a pool url
+                if (input.Contains("https://e621.net/pools/"))
+                {
+                    //get pool id from url
+                    string poolID = input.Substring(20);
+                    getPool(Convert.ToInt32(poolID));
+                }
+                else if (input.Contains("e621.net/pools/"))
+                {
+                    //get pool id from url (short)
+                    string poolID = input.Substring(12);
+                    getPool(Convert.ToInt32(poolID));
+                }
+                else
+                {
+                    //input is a pool id
+                    getPool(Convert.ToInt32(input));
+                }
+            }
+
+        }
+
+        private static void getPool(int poolID)
+        {
+            WebClient client = new WebClient();
+            client.Headers.Clear();
+            client.Headers.Add("user-agent", "PoolDownloaderNET/0.01 (by NotVila on e621)");
+
+            try
+            {
+            string json = client.DownloadString($"https://e621.net/pools/{poolID}.json");
+
+            var pool = JsonSerializer.Deserialize<GetJson>(json);
+
+            string title = pool.name.Replace("_", " ");
+            string creator = pool.creator_name.Replace("_", " ");
+
+            Console.WriteLine($"Downloading: {title} by {creator}");
+
+            downloadPool(pool.post_ids, pool);
+            }
+            catch (WebException)
+            {
+                Console.WriteLine("\nPool not found");
+                Thread.Sleep(500);
+                Console.Clear();
+                userInput();
+            }
+        }
+
+        private static void downloadPool(int[] post_ids, GetJson pool)
+        {
+            var num = 1;
+            WebClient client = new WebClient();
+            client.Headers.Clear();
+            client.Headers.Add("user-agent", "PoolDownloaderNET/0.01 (by NotVila on e621)");
+
+            int[] postIDs = new int[pool.post_ids.Length];
+
+            for (int i = 0; i < pool.post_ids.Length; i++)
+            {
+                
+                postIDs[i] = pool.post_ids[i];
+                Console.WriteLine($"Downloading post {i + 1} of {pool.post_ids.Length}");
+                
+                client.Headers.Clear();
+                client.Headers.Add("user-agent", "PoolDownloaderNET/0.01 (by NotVila on e621)");
+
+                string jsonFile = client.DownloadString($"https://e621.net/posts/{postIDs[i]}.json");
+
+                var File = JsonSerializer.Deserialize<Sample>(jsonFile);
+
+                Console.WriteLine(File.url);
+
+                Console.WriteLine($"https://e621.net/posts/{postIDs[i]}.json");
+
+
+                //client.DownloadFile($"{posts.url}", $"./{name.id}.{posts.ext}");
+                num++;
+
+                ////skip if value is null
+                //if (posts.url is null)
+                //{
+                //    Console.WriteLine("Skipping");
+                //    continue;
+                //}
+                //else
+                //{
+                //    //download file, save in pool folder
+
+                //}
+            }
+
+        }
+    }
+}
